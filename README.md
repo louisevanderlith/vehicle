f01320c5321aadcd66fa49b813fa620e44092e31
Louise van der Lith <louisevanderlith@gmail.com>
2019-06-03 22:58:48 +0200

    Removed Runmode, build fixed

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Vehicle is where you get the full information of a car.
 ## Run with Docker
 * $ docker build -t avosa/vehicle:dev .
 * $ docker rm VehicleDEV
-* $ docker run -d -e RUNMODE=DEV -p 8098:8098 --network mango_net --name VehicleDEV avosa/vehicle:dev 
+* $ docker run -d -p 8098:8098 --network mango_net --name VehicleDEV avosa/vehicle:dev 
 * $ docker logs VehicleDEV
--- routers/router.go
+++ routers/router.go
@@ -1,18 +1,20 @@
 package routers
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
 	"github.com/louisevanderlith/mango"
 	"github.com/louisevanderlith/mango/control"
 	"github.com/louisevanderlith/secure/core"
 	"github.com/louisevanderlith/secure/core/roletype"
-	"github.com/louisevanderlith/secure/logic"
 	"github.com/louisevanderlith/vehicle/controllers"
 )
 
-func Setup(s *mango.Service) {
-	ctrlmap := EnableFilter(s)
+func Setup(s *mango.Service, host string) {
+	ctrlmap := EnableFilter(s, host)
 
 	vehCtrl := controllers.NewVehicleCtrl(ctrlmap)
 
@@ -21,8 +23,8 @@ func Setup(s *mango.Service) {
 	beego.Router("/v1/vehicle/all/:pagesize", vehCtrl, "get:Get")
 }
 
-func EnableFilter(s *mango.Service) *control.ControllerMap {
-	ctrlmap := logic.NewMasterMap(s)
+func EnableFilter(s *mango.Service, host string) *control.ControllerMap {
+	ctrlmap := control.CreateControlMap(s)
 
 	emptyMap := make(core.ActionMap)
 	emptyMap["GET"] = roletype.User
@@ -30,14 +32,13 @@ func EnableFilter(s *mango.Service) *control.ControllerMap {
 
 	ctrlmap.Add("/v1/vehicle", emptyMap)
 
-	beego.InsertFilter("/v1/*", beego.BeforeRouter, ctrlmap.FilterAPI)
+	beego.InsertFilter("/v1/*", beego.BeforeRouter, ctrlmap.FilterAPI, false)
+	allowed := fmt.Sprintf("https://*%s", strings.TrimSuffix(host, "/"))
 
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowAllOrigins: true,
-		AllowMethods:    []string{"GET", "POST", "OPTIONS"},
-		AllowHeaders:    []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Content-Type"},
-		ExposeHeaders:   []string{"Content-Length", "Access-Control-Allow-Origin"},
-	}))
+		AllowOrigins: []string{allowed},
+		AllowMethods: []string{"GET", "POST", "OPTIONS"},
+	}), false)
 
-	return ctrlmap.ControllerMap
+	return ctrlmap
 }
