f18971992dc72037c33994aeee2c94bc65196673
louisevanderlith <louisevanderlith@gmail.com>
2020-09-14 12:11:56 +0200

    Added vehicle seed

--- core/dbcontext.go
+++ core/dbcontext.go
@@ -1,7 +1,11 @@
 package core
 
 import (
+	"encoding/json"
 	"github.com/louisevanderlith/husk"
+	"github.com/louisevanderlith/husk/collections"
+	"os"
+	"reflect"
 )
 
 type context struct {
@@ -11,9 +15,42 @@ type context struct {
 var ctx context
 
 func CreateContext() {
+	defer seed()
 	ctx = context{
 		Vehicles: husk.NewTable(Vehicle{}),
 	}
+}
+
+func seed() {
+	profiles, err := vehicleSeed()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = ctx.Vehicles.Seed(profiles)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func vehicleSeed() (collections.Enumerable, error) {
+	f, err := os.Open("db/vehicles.seed.json")
+
+	if err != nil {
+		return nil, err
+	}
+
+	var items []Vehicle
+	dec := json.NewDecoder(f)
+	err = dec.Decode(&items)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return collections.ReadOnlyList(reflect.ValueOf(items)), nil
 }
 
 func Shutdown() {
