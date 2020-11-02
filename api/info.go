1392baaaa140516796261b4d489c03e4fb40b0d7
louisevanderlith <louisevanderlith@gmail.com>
2020-11-02 19:51:45 +0200

    Updated API

--- api/info.go
+++ api/info.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/louisevanderlith/husk/hsk"
+	"github.com/louisevanderlith/husk/records"
 	"github.com/louisevanderlith/vehicle/core"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -18,7 +20,34 @@ func FetchVehicleInfo(web *http.Client, host string, k hsk.Key) (core.Vehicle, e
 
 	defer resp.Body.Close()
 
+	if resp.StatusCode != http.StatusOK {
+		bdy, _ := ioutil.ReadAll(resp.Body)
+		return core.Vehicle{}, fmt.Errorf("%v: %s", resp.StatusCode, string(bdy))
+	}
+
 	result := core.Vehicle{}
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&result)
+
+	return result, err
+}
+
+func FetchAllVehicles(web *http.Client, host, pagesize string) (records.Page, error) {
+	url := fmt.Sprintf("%s/info/%s", host, pagesize)
+	resp, err := web.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bdy, _ := ioutil.ReadAll(resp.Body)
+		return nil, fmt.Errorf("%v: %s", resp.StatusCode, string(bdy))
+	}
+
+	result := records.NewResultPage(core.Vehicle{})
 	dec := json.NewDecoder(resp.Body)
 	err = dec.Decode(&result)
 
